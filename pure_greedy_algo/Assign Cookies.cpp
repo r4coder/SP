35bc9f080f7f01585336b753c285a9219b268f63
r4coder <ritheeshk2003@gmail.com>
2025-12-13 22:04:55 +0530

    Add greedy algorithm for Assign Cookies problem
    Implement a greedy algorithm to maximize content children based on cookie sizes.

--- pure_greedy_algo/Assign Cookies.cpp
+++ pure_greedy_algo/Assign Cookies.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
 
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int g_s=g.size();
+        int s_s=s.size();
+        int i=g_s-1;
+        int j=s_s-1;
+        int count=0;
+
+
+        while(i>=0&&j>=0)
+        {
+          if(s[j]>=g[i])
+          {
+                count++;
+                j--;
+                i--; 
+          }
+
+          else
+          {
+            i--;
+          }
+
+        }
+
+        return count;
+
+
+        
+    }
+};
